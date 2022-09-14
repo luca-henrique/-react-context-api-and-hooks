49f9095d3a8562f32b6e2ea3e7785af347d6c809
Lucas Henrique <lukas.paes18@gmail.com>
2022-09-14 16:43:59 -0300

    feature: actions types counter context

---
+++ src/contexts/CounterContext/actions-types.js
@@ -0,0 +1,10 @@
+const prefix = `CounterContext`;
+
+export const INCREASE = `${prefix}/INCREASE`;
+export const DECREASE = `${prefix}/DECREASE`;
+export const RESET = `${prefix}/RESET`;
+export const SET_COUNTER = `${prefix}/SET_COUNTER`;
+
+export const ASYNC_INCREATE_START = `${prefix}/ASYNC_INCREATE_START`;
+export const ASYNC_INCREATE_END = `${prefix}/ASYNC_INCREATE_END`;
+export const ASYNC_INCREATE_ERROR = `${prefix}/ASYNC_INCREATE_ERROR`;
